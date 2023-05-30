4af3f3262e8b209c61f6e1dd928633100994f5b2
Juan Torres <juane@backbase.com>
2023-05-30 16:53:17 -0500

    Added Email settings and env

--- src/config/config.js
+++ src/config/config.js
@@ -24,5 +24,7 @@ export default {
     mongoUrl: process.env.MONGO_URL,
     adminName: process.env.ADMIN_NAME,
     adminPassword: process.env.ADMIN_PASSWORD,
+    gmailAccount: process.env.GMAIL_ACCOUNT,
+    gmailAppPassword: process.env.GMAIL_APP_PASSWD,
     environment: environment
 };
